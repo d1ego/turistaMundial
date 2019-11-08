be0e81830a11fffe1dff87a9e59b530786904df7
Osvaldo2212 <48491506+Osvaldo2212@users.noreply.github.com>
2019-11-08 10:53:13 -0600

    Update Casilla.h

--- Casilla.h
+++ Casilla.h
@@ -8,10 +8,12 @@ class Casilla
 		int posicion;
 		int precio;
 		int estado;
+		int propietario;
 	public:
-		Casilla(string a, int pos, int pre, int est);
+		Casilla(string a, int pos, int pre, int est, int prop);
 		void Setprecio(int a);
 		void Setestado(int a);
+		void Casilla::Setpropietario(int pro);
 		string Getnombre();
 		int Getposicion();
 		int Getprecio();
