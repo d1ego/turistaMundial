550c3b8caed4f8c418b6585f802ce0f5259c33d0
d1ego <dipackmon@gmail.com>
2019-11-14 17:47:13 -0600

    Create opJugador.cpp

---
+++ opJugador.cpp
@@ -0,0 +1,18 @@
+#include "opJugador.h"
+
+oPjugador::
+
+class opJugador
+  {
+    private:
+      Jugador jugador;
+    public:
+      void pagar(int posicionCasilla);
+      void mover(int posicionCasilla, int idJugador);
+      void hipotecar(int posicionCasilla, int idJugador);
+      void tirar(int idJugador);
+      void construir(int id Jugador, int posicionCasilla);
+      void vender(int id Jugador, int posicionCasilla);
+  };
+  
+  
