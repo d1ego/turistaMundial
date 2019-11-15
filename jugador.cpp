28309979721097f00af510ed6db731301f2b08c1
d1ego <dipackmon@gmail.com>
2019-11-14 18:11:51 -0600

    Update jugador.cpp

--- jugador.cpp
+++ jugador.cpp
@@ -1,6 +1,8 @@
+#include<time.h>
+#include<stdlib.h>
 #include "jugador.h"
 
-Jugador::Jugador(int id, int dinero, int posicion): id(id), dinero(dinero), posicion(posicion)
+Jugador::Jugador(int id, string nickname, int dinero, int posicion)
 {
 
 }
@@ -19,3 +21,53 @@ string Jugador::getPosicion()
 {
 	return posicion;
 }
+
+int Jugador::sacar(int id, int dadoA, int dadoB)
+{
+	srand(time(NULL));
+	dadoA = rand();
+	dadoB = rand();
+	dadoA=1+rand()%(13-1);
+	dadoB=1+rand()%(13-1);
+}
+void comprar()
+{
+}	
+void vender()
+{
+}
+void terminaTurno()
+{
+}
+void hipotecar()
+{
+}
+void pagar()
+{
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+class Jugador
+{
+	private:
+		int id;
+		string nickname;
+		int dinero;
+		int posicion;
+        
+        //listado de propiedades??
+	public:
+        	Jugador(int id, string nickname, int dinero, int posicion);  
+        	void setId();
+        	int getDinero();
+        	void getPosicion();
+		void sacar();
+		void comprar();
+		void vender();
+		void terminaTurno();
+		void hipotecar();
+		void pagar();
+		
+};*/
